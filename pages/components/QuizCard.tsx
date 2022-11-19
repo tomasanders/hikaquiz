b4fd509a4dab936979a5f91b1814132b9b4093b6
Tom Sanders <thomas.a.sanders@gmail.com>
2022-11-19 15:46:58 +0900

    Added counter logic

--- pages/components/QuizCard.tsx
+++ pages/components/QuizCard.tsx
@@ -2,15 +2,25 @@ import { quizList } from "../javascript/quizLogic";
 import { useState, useRef } from "react";
 
 export const QuizCard = () => {
+  const [question, setQuestion] = useState();
+  const [answer1, setAnswer1] = useState();
+  const [answer2, setAnswer2] = useState();
+  const [answer3, setAnswer3] = useState();
+  const [answer4, setAnswer4] = useState();
+  const [quizStarted, setQuizStarted] = useState(false);
+
+  // This sets the initial state of the quizHandler, based on quizList.
   const quizHandler = () => {
     setQuestion(quizList[0][0]);
     setAnswer1(quizList[0][2][0]);
     setAnswer2(quizList[0][2][1]);
     setAnswer3(quizList[0][2][2]);
     setAnswer4(quizList[0][2][3]);
+    setQuizStarted(true);
   };
 
-  const counter = useRef(0);
+  // This increments whenever an answer is processed, and moves to the next question in the quizList.
+  const counter = useRef(0); // useRef to preserve value between refreshes
   const [count, setCount] = useState(counter.current);
 
   const increment = () => {
@@ -29,11 +39,6 @@ export const QuizCard = () => {
   };
 
 
-  const [question, setQuestion] = useState();
-  const [answer1, setAnswer1] = useState();
-  const [answer2, setAnswer2] = useState();
-  const [answer3, setAnswer3] = useState();
-  const [answer4, setAnswer4] = useState();
   return (
     <div className="flex flex-col items-center justify-center bg-slate-300 p-4 gap-4 w-full">
       <button className="px-4 py-2 bg-purple-400 border border-solid border-purple-800 rounded" onClick={quizHandler}>Start Quiz</button>
@@ -48,6 +53,7 @@ export const QuizCard = () => {
         <button className="p-4 bg-blue-200 border border-solid border-blue-800 rounded w-full">{answer3}</button>
         <button className="p-4 bg-blue-200 border border-solid border-blue-800 rounded w-full">{answer4}</button>
       </div>
+      {quizStarted && <p className="w-100 text-center">Question: {count + 1}/{quizList.length}</p>}
     </div>
   );
 };
