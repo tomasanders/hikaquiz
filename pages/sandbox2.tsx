5ae42ca403a5e0a49cedcac7502beaad336287ae
Tom Sanders <thomas.a.sanders@gmail.com>
2022-11-26 15:33:58 +0900

    style reset

--- pages/sandbox2.tsx
+++ pages/sandbox2.tsx
@@ -5,14 +5,30 @@ import { quizList } from "../src/javascript/QuizLogic";
 const TestPage: NextPage = (props) => {
   const [batchSize, setBatchSize] = useState(4); // Sets the number of possible answer tiles.
   const [quizStarted, setQuizStarted] = useState(false); // Sets the quiz as started/not started.
+  const [quizContent, setQuizContent] = useState('hiragana'); // Sets the quiz content.
 
 
 
   return (
-    <div className="quiz-wrap">
+    <div>
       <h1 className="title">HiKa Quiz</h1>
 
-      { quizStarted === false && <p>Choose your options:</p>}
+      { quizStarted === false &&
+        <div className="options-menu">
+          <p>Choose your options:</p>
+          <p>How many possible answers would you like?</p>
+          <div className="options-btn-wrap">
+            <button onClick={() => setBatchSize(4)} className={`${batchSize === 4 ? 'bg-correct' : 'bg-default'}`}>4</button>
+            <button onClick={() => setBatchSize(8)} className={`${batchSize === 8 ? 'bg-correct' : 'bg-default'}`}>8</button>
+          </div>
+          <p>What would you like to quiz on?</p>
+          <div className="options-btn-wrap">
+            <button onClick={() => setQuizContent('hiragana')} className={`${quizContent === 'hiragana' ? 'bg-correct' : 'bg-default'}`}>Hiragana</button>
+            <button onClick={() => setQuizContent('katakana')} className={`${quizContent === 'katakana' ? 'bg-correct' : 'bg-default'}`}>Katakana</button>
+            <button onClick={() => setQuizContent('both')} className={`${quizContent === 'both' ? 'bg-correct' : 'bg-default'}`}>Both</button>
+          </div>
+        </div>
+      }
 
       {/* Question Frame */}
       { quizStarted === true &&
