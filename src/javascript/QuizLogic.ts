210fa7a9046d1a35e4b429264b54b26b104654b4
Tom Sanders <thomas.a.sanders@gmail.com>
2022-11-26 17:06:21 +0900

    updated start logic

--- src/javascript/QuizLogic.ts
+++ src/javascript/QuizLogic.ts
@@ -42,10 +42,22 @@ const createQuiz = (array: string[][], count: number) => {
 /**
  * Starts the quiz - prepares all the data
  */
-const startQuiz = (array: string[][], batchSize: number) => {
-  const shuffledData: string[][] = getShuffledArray(array);
-  const quizList: string[] | string[][] = createQuiz(shuffledData, batchSize);
-  return quizList;
+export const prepareQuiz = (content: string, batchSize: number) => {
+  if (content === "both") {
+    const combinedKana = hiragana.concat(katakana);
+    const shuffledData: string[][] = getShuffledArray(combinedKana);
+    const quizList: string[] | string[][] = createQuiz(shuffledData, batchSize);
+    console.log(quizList, "bothList")
+    return quizList;
+  } else if (content === "hiragana") {
+    const shuffledData: string[][] = getShuffledArray(hiragana);
+    const quizList: string[] | string[][] = createQuiz(shuffledData, batchSize);
+    console.log(quizList, "hiraganaList")
+    return quizList;
+  } else if (content === "katakana") {
+    const shuffledData: string[][] = getShuffledArray(katakana);
+    const quizList: string[] | string[][] = createQuiz(shuffledData, batchSize);
+    console.log(quizList, "katakanaList")
+    return quizList;
+  };
 };
-
-export const quizList = startQuiz(hiragana, 3);
